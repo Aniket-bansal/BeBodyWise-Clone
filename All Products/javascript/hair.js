b99704b19713f8363b75d528bc49604ab492d421
shaileshwebdeveloper <91751062+shaileshwebdeveloper@users.noreply.github.com>
2022-04-01 19:23:43 +0530

    Add files via upload

---
+++ All Products/javascript/hair.js
@@ -0,0 +1,293 @@
+     var productData = [
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1613991979/staging/products/hair-health-gummies/Carousel%20NEW/Hair_white-BG.jpg",
+          name: "Biotin Hair Gummies (30)",
+          price: "₹499",
+          uses: "Hair growth & strength",
+          details: "Biotin 5000 MCG, Multivitamins, Zinc",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1621853749/staging/products/Hair%20Growth%20Concentrate/Carousel%20NEW/1000x750.jpg",
+          name: "Hair Growth Serum",
+          price: "₹799",
+          uses: "Hair Growth",
+          details: "3% Redensyl, 3% Procapil, 2% Baicapil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1620821626/staging/products/hair-health-gummies/Hair%20Gummies%2060%20Pack/hair_1600x1200.jpg",
+          name: "Biotin Hair Gummies (60)",
+          price: "₹899",
+          uses: "Hair growth & strength",
+          details: "Biotin 5000 MCG, Multivitamins, Zinc",
+        }, 
+
+        
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1645458145/staging/products/Anti%20dandruff%20lotion/0_UPDATED/PRODUCT%20IMAGES/ADL_1000x750.png",
+          name: "Anti Dandruff Lotion",
+          price: "₹349",
+          uses: "Instant Dandruff Control",
+          details: "ICHTHYOL® PALE, Zinc Pyrithione, Menthol",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1638857946/staging/products/effervescent-biotin-tablets/PACK%20OF%202/pack-of-2.png",
+          name: "Hair Growth Serum",
+          price: "₹499",
+          uses: "Hair growth, Hair health, Hair fall",
+          details:  "Biotin, Sesbania Extract, Beta-sitosterol",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1643995764/staging/products/1-ketoconazole-dandruff-shampoo/1_PHOTO/PRODUCT%20IMAGES/1KDS_1000x750.jpg",
+          name: "1% Ketoconazole Dandruff Shampoo",
+          price: "₹299",
+          uses: "Dandruff Control",
+          details:  "1% Ketoconazole, Salicylic Acid",
+        }, 
+
+
+
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/staging/app/category_images/cat%20display%20units%20images/hair_oil.jpg",
+          name: "Onion Hair Growth Oil",
+          price: "₹399",
+          uses: "Hair growth and strengthening",
+          details: "Onion oil, Castor oil, Olive oil, Amla oil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/staging/products/hair-strengthening-serum/1_Product_Image_2.jpg",
+          name: "Hair Strengthening Serum",
+          price: "₹449",
+          uses: "Frizzy, unruly hair",
+          details: "Argan oil, Olive oil, Sunflower oil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1643983066/staging/products/shampoo-pdp/1_PHOTO/CAROUSEL/HFCS_0.jpg",
+          name: "Keratin Shampoo for Hair Fall",
+          price: "₹449",
+          uses: "Hair fall reduction",
+          details: "Keratin, Biotin, Niacin, Panthenol",
+        }, 
+
+
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/staging/app/category_images/cat%20display%20units%20images/hair_oil.jpg",
+          name: "Onion Hair Growth Oil",
+          price: "₹399",
+          uses: "Hair growth and strengthening",
+          details: "Onion oil, Castor oil, Olive oil, Amla oil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/staging/products/hair-strengthening-serum/1_Product_Image_2.jpg",
+          name: "Hair Strengthening Serum",
+          price: "₹449",
+          uses: "Frizzy, unruly hair",
+          details: "Argan oil, Olive oil, Sunflower oil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1643983066/staging/products/shampoo-pdp/1_PHOTO/CAROUSEL/HFCS_0.jpg",
+          name: "Keratin Shampoo for Hair Fall",
+          price: "₹449",
+          uses: "Hair fall reduction",
+          details: "Keratin, Biotin, Niacin, Panthenol",
+        }, 
+
+   
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1602319806/staging/products/Conditioner%20-pdp/1600x1200.png",
+          name: "Onion Hair Growth Oil",
+          price: "₹399",
+          uses: "Dry, damaged, dehydrated hair",
+          details: "Silk Protein, Biotin, Niacinamide, Shea Butter",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1640335849/staging/products/darkenyl-hair-serum/PRODUCT%20IMAGES/1DHS_1000x750.jpg",
+          name: "1% Darkenyl Hair Serum",
+          price: "₹899",
+          uses: "Reducing Premature Greying",
+          details: "1% Darkenyl, Bhringraj, Amla",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1638940998/staging/products/redensyl-hair-oil/PRODUCT%20IMAGES/1RHO_1000x750.jpg",
+          name: "1% Redensyl Hair Oil",
+          price: "₹549",
+          uses: "Hair growth, Reduced Hair Fall",
+          details: "1% Redensyl, Onion oil, Bhringraj",
+        }, 
+
+
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1637851321/staging/products/effervescent-biotin-tablets/PRODUCT%20IMAGES/1000x750_EBT.jpg",
+          name: "Onion Hair Growth Oil",
+          price: "₹299",
+          uses: "Hair growth, Hair health, Hair fall",
+          details: "Biotin, Sesbania Extract, Beta-sitosterol",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1602753797/staging/products/minoxidil-topical/1600x1200_2___.png",
+          name: "Minoxidil Topical Solution USP 5% w/v",
+          price: "₹699",
+          uses: "Bald spots, Excessive hair fall",
+          details: "5% Minoxidil, Procapil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1642602143/staging/products/chelate-water-softener/PRODUCT%20IMAGES/CWS_1000x750.png",
+          name: "Chelate Water Softener",
+          price: "₹449",
+          uses: "Softening Hard Water",
+          details: "Chelating Agents",
+        }, 
+
+
+
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1618881636/staging/products/buying-options/Hair%20Pro-Growth%20Pack/New%20Carousel/Hair_Growth_Pack.jpg",
+          name: "Hair Growth Pack",
+          price: "₹1199",
+          uses: "New hair growth",
+          details: "3% Redensyl Solution, 5000 MCG Biotin Gummies",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1604072938/staging/products/buying-options/Hair%20Restore%20Edition/Hair_Restore_CAT_PAGE.png",
+          name: "Hair Restore Edition",
+          price: "₹1499",
+          uses: "Hair Fall Control",
+          details: "Silk Protein Conditioner, Biotin Shampoo , Onion Hair Oil",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1620047677/staging/products/buying-options/Hair%20Health%20Starter%20Pack/New%20Carousel/Hair_Health_Starter_Pack.jpg",
+          name: "Hair Health Starter Pack% Redensyl Hair Oil",
+          price: "₹799",
+          uses: "Hair Health, Hair Growth",
+          details: "Biotin Shampoo , 5000 MCG Biotin Gummies (30)",
+        }, 
+
+
+
+
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1640771666/staging/products/buying-options/Anti%20Dandruff%20Starter%20Pack/CAROUSEL/ADSP_1000x750.jpg",
+          name: "Anti Dandruff Starter Pack",
+          price: "₹599",
+          uses: "Dandruff Control",
+          details: "1% Keto Shampoo, Anti dandruff Lotion",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1619428969/staging/products/buying-options/Hair%20fall%20Control%20Mini/New%20Carousel/Hair_Fall_Control_Beginner_s_Pack.jpg",
+          name: "Hair Fall Control Mini",
+          price: "₹799",
+          uses: "Frizz control , Hair Fall control",
+          details: "Biotin Shampoo, Silk Protein Conditioner",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1620062959/staging/products/buying-options/Hair%20fall%20Control%20Starter%20Pack/New%20Carousel/Hair_fall_Control_Starter_Pack.jpg",
+          name: "Hair Fall Control Starter Pack",
+          price: "₹849",
+          uses: "Hair Growth & Strengthening",
+          details: "Onion Hair Oil , Biotin Shampoo",
+        }, 
+
+
+
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1641478408/staging/products/buying-options/dandruff%20starter%20kit/carousel/DSK_1000x750.png",
+          name: "Dandruff Starter Kit",
+          price: "₹699",
+          uses: "Dandruff Control & Hair Strength",
+          details: "1% Keto Shampoo, 5000 MCG Biotin Gummies",
+        },
+        {
+          image_url:
+            "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1641480325/staging/products/buying-options/instant%20dandruff%20relief%20kit/carousel/IDRK_1000x750_N.jpg",
+          name: "Instant Dandruff Relief Kit",
+          price: "₹649",
+          uses: "Dandruff Control, Frizz control",
+          details: "1% Keto Shampoo, Silk Protein Conditioner",
+        },
+
+      ];
+
+
+
+    productData.map(function(elem)
+    {
+        var box = document.createElement("div")
+    
+        var img = document.createElement("img")
+        img.src = elem.image_url
+    
+        var name = document.createElement("p")
+        name.textContent=elem.name
+    
+        var price = document.createElement("p")
+        price.innerText = elem.price
+
+        var use =  document.createElement("p")
+        use.innerText = elem.uses
+        use.setAttribute("id", "word1");
+
+        var details=  document.createElement("p")
+        details.innerText = elem.details
+        details.setAttribute("id", "word2");
+
+        var btn = document.createElement("button")
+        btn.innerText = "Add to Cart"
+        
+    
+        box.append(img,name,price, use, details, btn)
+    
+        document.querySelector("#allproducts").append(box)
+    
+    });
+    
+
+    document.querySelector("#hair").addEventListener("click", hairfunction)
+    function hairfunction(){
+        
+        window.location.href = "hair.html"
+    }
+
+    document.querySelector("#skin").addEventListener("click", skinfunction)
+    function skinfunction(){
+        window.location.href = "skin.html"
+    }
+
+    document.querySelector("#weight").addEventListener("click", weightfunction)
+    function weightfunction(){
+        window.location.href = "weight.html"
+    }
+
+    document.querySelector("#pcos").addEventListener("click", pcosfunction)
+    function pcosfunction(){
+        window.location.href = "pcos.html"
+    }
+
+    document.querySelector("#feminine").addEventListener("click", femfunction)
+    function femfunction(){
+        window.location.href = "feminine.html"
+    }
+    
---
+++ All Products/javascript/skin.js
@@ -0,0 +1,268 @@
+var productData = [
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1636804535/staging/products/skin-clarifying/0_BLUE/PRODUCT%20IMAGES/10NS_1000x750.jpg",
+    name: "10% Niacinamide Serum",
+    price: "₹489",
+    uses: "Acne Marks, Hyperpigmentation",
+    details: "10% Niacinamide, 1% Zinc PCA",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1635251558/staging/products/skin-rejuvenation-serum/0_BLUE/PRODUCT%20IMAGES2/20VitC_1000x750.png",
+    name: "20% Vitamin C Face Serum",
+    price: "₹599",
+    uses: "Glowing & Brightening Skin",
+    details: "20% Vitamin C, Ferulic Acid, Glycolic Acid",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1641809040/staging/products/1-salicylic-acid-body-wash/PRODUCT%20IMAGES/1SABW_1000x750.jpg",
+    name: "1% Salicylic Acid Body Wash for Women",
+    price: "₹349",
+    uses: "Cleansing Skin, Preventing Body Acne",
+    details: "1% Salicylic acid, Chamomile Extract",
+  }, 
+
+  
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1638247817/staging/products/skin-health-gummies/0_BLUE/PRODUCT%20IMAGES/1000x750_SHG.png",
+    name: "Anti Dandruff Lotion",
+    price: "₹349",
+    uses: "Instant Dandruff Control",
+    details: "ICHTHYOL® PALE, Zinc Pyrithione, Menthol",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1638891793/staging/products/skin-health-gummies/0_BLUE/Pack%20of%2060/CAROUSEL/0.png",
+    name: "Skin Health Gummies (60)",
+    price: "₹999",
+    uses: "Dull skin",
+    details:  "Hyaluronic Acid, Curcumin, Vit C & E",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1636791635/staging/products/collagen-skin-gummies/PRODUCT%20IMAGES/1000x750_N.png",
+    name: "Collagen Skin Gummies",
+    price: "₹799",
+    uses: "Fine Lines, Hydration",
+    details:  "Sesbania Extract, Collagen Boosting Blend, Multivitamin",
+  }, 
+
+
+
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1615635403/staging/products/adapalene-benzoyl/Carousel/Adapalene_BP.jpg",
+    name: "Adapalene and Benzoyl Peroxide Gel",
+    price: "₹399",
+    uses: "Active Acne",
+    details: "2.5% Benzoyl peroxide",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1648210348/staging/products/acne-spot-correction/0_2SAG/PRODUCT%20IMAGES/2SAG_1000x750.jpg",
+    name: "2% Salicylic Acid Gel",
+    price: "₹449",
+    uses: "Active Acne",
+    details: "2% Salicylic acid, 1% Glycolic acid",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1643013958/staging/products/acne-control-wash/New%20blue%20images/0_BRUSH/PRODUCT%20IMAGES/SAFW_1000x750.jpg",
+    name: "1% Salicylic Acid Foaming Face Wash",
+    price: "₹349",
+    uses: "Acne prone skin",
+    details: "1% Salicyclic acid, 1% Glycolic acid",
+  }, 
+
+
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1610589752/staging/products/oil-control-moisturiser/Oil-Control-Moisturiser-Tube-with-monocarton_1600X1200.jpg",
+    name: "Oil Control Moisturizer",
+    price: "₹297",
+    uses: "Acne-prone skin",
+    details: "Ceramides, Phytosqualene, Hyaluronic acid, Arginine",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1639571842/staging/products/collagen-powder/PRODUCT%20IMAGES/CP_1000x750.jpg",
+    name: "Collagen Powder",
+    price: "₹799",
+    uses: "Fine Lines, Hydration",
+    details: "Glycine & Proline, Omega 3, Multivitamins",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1636697869/staging/products/2-salicylic-acid-serum/PRODUCT%20IMAGES/2SAFS_1000x750.jpg",
+    name: "2% Salicylic Acid Face Serum",
+    price: "₹549",
+    uses: "Hair fall reduction",
+    details: "WITH2% Salicylic Acid, 2% Fermented Rice Water, 0.5% Niacinamide",
+  }, 
+
+
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1641555836/staging/products/2-salicylic-acid-face-wash/PRODUCT%20IMAGES/2SAFW_1000x750.png",
+    name: "2% Salicylic Acid Face Wash",
+    price: "₹299",
+    uses: "Acne-Prone skin",
+    details: "2% Salicyclic acid, Allantoin",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1622686002/staging/products/Pore%20Tightening%20Toner/Carousel/1600x1200.jpg",
+    name: "Pore Tightening Toner",
+    price: "₹499",
+    uses: "Large open pores",
+    details: "5% Lactic Acid, Arginine, Witch Hazel, Aloe Vera",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1611886096/staging/products/Skin%20Hydrating%20Cleanser/Carousel/1600x1200.jpg",
+    name: "Hyaluronic Acid Face Wash",
+    price: "₹315",
+    uses: "Dry, sensitive skin",
+    details: "Pentavitin, Shea Butter, Hyaluronic Acid, Allantoin",
+  }, 
+
+
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1639978172/staging/products/Retinol%20Under%20Eye%20Cream/PRODUCT%20IMAGES/2RUEC_1000X750.jpg",
+    name: "2% Bio Retinol Under Eye Cream",
+    price: "₹399",
+    uses: "Dark Circles, Fine Lines & Wrinkles",
+    details: "2% Bio Retinol, 1% Niacinamide, 2% Kakadu Plum",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1637737681/staging/products/skin-regenerate-night/0_BLUE/PRODUCT%20IMAGES/1000x750.jpg",
+    name: "1% Retinol Liposome Night Cream",
+    price: "₹599",
+    uses: "Fine lines & wrinkles",
+    details: "1% Retinol, 3% Niacinamide",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1639097000/staging/products/Sunscreen/0_BLUE/PRODUCT%20IMAGES/BSS_1000x750.png",
+    name: "Broad Spectrum SPF 30 Sunscreen",
+    price: "₹399",
+    uses: "Sun & blue light protection",
+    details: "Avobenzone, Titanium Dioxide",
+  }, 
+
+
+
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1643027363/staging/products/buying-options/clear%20skin%20edition/0_BLUE/PRODUCT%20IMAGES/TCSE_1000x750_N.jpg",
+    name: "The Clear Skin Edition",
+    price: "₹1379",
+    uses: "Active acne, Acne scars",
+    details: "WITH10% Niacinamide Serum, AHA-BHA Facewash, AHA-BHA",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1643259335/staging/products/buying-options/Acne%20scar%20control%20starter%20pack/0_BLUE/PRODUCT%20IMAGES/AMRK_1000x750_N.jpg",
+    name: "Acne Marks Reduction Kit",
+    price: "799",
+    uses: "Acne scars",
+    details: "AHA-BHA Facewash, 10% Niacinamide Serum",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1634181333/staging/products/buying-options/Active%20Acne%20and%20Scar%20Removal%20Pack/0_BLUE/Acne%20Treatment%20Kit_1000X750.png",
+    name: "Acne Treatment Kit",
+    price: "₹899",
+    uses: "Active acne, Acne scars",
+    details: "10% Niacinamide Serum, AHA-BHA Gel",
+  }, 
+
+
+
+
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1639457747/staging/products/buying-options/Skin%20Glow%20Starter%20Pack/0_BLUE/PRODUCT%20IMAGES/SGSP_1000x750.jpg",
+    name: "Skin Glow Starter Pack",
+    price: "₹1099",
+    uses: "Bright & Glowing skin",
+    details: "20% Vitamin C Serum, Skin Health Gummies",
+  },
+  {
+    image_url:
+      "https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_800,c_limit/v1639450858/staging/products/buying-options/Anti-aging%20starter%20pack/0_BLUE/PRODUCT%20IMAGES/SIK_1000x750.jpg",
+    name: "Skin Illuminating Kit",
+    price: "₹1149",
+    uses: "Dullness, Fine lines & wrinkles",
+    details: "20% Vitamin C Serum, 1% Retinol Liposome Night Cream",
+  },
+ 
+
+];
+
+
+
+productData.map(function(elem)
+{
+  var box = document.createElement("div")
+
+  var img = document.createElement("img")
+  img.src = elem.image_url
+
+  var name = document.createElement("p")
+  name.textContent=elem.name
+
+  var price = document.createElement("p")
+  price.innerText = elem.price
+
+  var use =  document.createElement("p")
+  use.innerText = elem.uses
+  use.setAttribute("id", "word1");
+
+  var details=  document.createElement("p")
+  details.innerText = elem.details
+  details.setAttribute("id", "word2");
+
+  var btn = document.createElement("button")
+  btn.innerText = "Add to Cart"
+  
+
+  box.append(img,name,price, use, details, btn)
+
+  document.querySelector("#allproducts").append(box)
+
+});
+
+
+    document.querySelector("#hair").addEventListener("click", hairfunction)
+    function hairfunction(){
+        
+        window.location.href = "hair.html"
+    }
+
+    document.querySelector("#skin").addEventListener("click", skinfunction)
+    function skinfunction(){
+        window.location.href = "skin.html"
+    }
+
+    document.querySelector("#weight").addEventListener("click", weightfunction)
+    function weightfunction(){
+        window.location.href = "weight.html"
+    }
+
+    document.querySelector("#pcos").addEventListener("click", pcosfunction)
+    function pcosfunction(){
+        window.location.href = "pcos.html"
+    }
+
+    document.querySelector("#feminine").addEventListener("click", femfunction)
+    function femfunction(){
+        window.location.href = "feminine.html"
+    }
+    
+
